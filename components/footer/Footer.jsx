aa4cd2c36251043e60e75978c8d43ec44f8f1440
Aya <pyon220@gmail.com>
2023-08-29 01:01:32 -0700

    modified footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -4,15 +4,7 @@ const Footer = () => {
       <div className="container">
         <p className="text-center py-6 text-gray-lite  dark:text-color-910 ">
           &copy; {new Date().getFullYear()} All Rights Reserved by{" "}
-          <a
-            className="hover:text-[#FA5252] duration-300 transition"
-            href="https://themeforest.net/user/ib-themes"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ib-themes
-          </a>
-          .
+          <a>Aya Ishimura</a>.
         </p>
       </div>
     </footer>
