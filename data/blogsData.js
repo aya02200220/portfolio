534722bc7bffbdd911244a833a356ae7f211d500
Aya <pyon220@gmail.com>
2024-08-06 01:27:08 -0700

    added ja to blog datas

--- data/blogsData.js
+++ data/blogsData.js
@@ -1,59 +1,119 @@
-module.exports = [
-  {
-    id: "5",
-    img: "/images/blog_images/5.jpg",
-    imgSmall: "/images/blog_images/small/5.jpg",
-    date: "Sep 16, 2023",
-    title:
-      "Overcoming Deployment Challenges with a MERN Stack application on Vercel",
-    bg: "#F1EBEC",
-    description:
-      "Deploying a MERN stack application, especially the server-side code, on platforms like Vercel can be a daunting task. Navigating through the myriad of configurations and dealing with unexpected issues can be overwhelming. In this article, I'll share the challenges I faced while deploying my MERN blog site's server-side code on Vercel and how I overcame them. ",
-    link: "https://dev.to/aya02200220/overcoming-deployment-challenges-with-a-mern-stack-blog-on-vercel-3p12",
-  },
-  {
-    id: "1",
-    img: "/images/blog_images/1.jpg",
-    imgSmall: "/images/blog_images/small/1.jpg",
-    date: "Aug 22, 2023",
-    category: "Inspiration",
-    title: "Why Isn't the Token Being Saved in the Cookie?",
-    bg: "#D5C5C8",
-    description:
-      "In web development, we occasionally come across issues that are puzzling and time-consuming to solve. One such issue that I recently faced involved JWT tokens not being stored in cookies for specific users. I invested several days trying to resolve this issue and eventually found a solution. In this blog post, I will share the details of the problem and the steps I took to resolve it.",
-    link: "https://dev.to/aya02200220/why-isnt-the-token-being-saved-in-the-cookie-1dmp",
-  },
-  {
-    id: "2",
-    img: "/images/blog_images/2.jpg",
-    imgSmall: "/images/blog_images/small/2.jpg",
-    date: "Jul 12, 2023",
-    title: "Quick Tips: Styling MUI Components with the sx Prop",
-    bg: "#F1EBEC",
-    description:
-      "Elevate Your MUI Components: Mastering the sx Prop for Styling Material-UI (now known as MUI) has always been a popular choice among React developers for its rich component library and flexibility. One of the game-changers introduced in MUI is the sx prop, which provides a powerful and concise way to style components directly. In today's quick tip, we'll explore how to use the sx prop to change button hover effects and to integrate images seamlessly into Box components.",
-    link: "https://dev.to/aya02200220/material-ui-1k03",
-  },
-  {
-    id: "3",
-    img: "/images/blog_images/3.jpg",
-    imgSmall: "/images/blog_images/small/3.jpg",
-    date: "Feb 10, 2023",
-    title: "Mastering the Basics: Manipulating the DOM with JavaScript",
-    bg: "#D5C5C8",
-    description:
-      "The Document Object Model (DOM) is a programming interface for HTML and XML documents that allows developers to manipulate web pages dynamically with JavaScript. By representing an HTML document as a tree structure, the DOM provides a convenient way to traverse, select, and modify elements on the page. In this article, we will cover the basics of how to manipulate the DOM with JavaScript. ",
-    link: "https://dev.to/aya02200220/we-have-to-kill-our-impostor-syndrome--40c3",
-  },
-  {
-    id: "4",
-    img: "/images/blog_images/4.jpg",
-    imgSmall: "/images/blog_images/small/4.jpg",
-    date: "Aug 31, 2022",
-    title: "About Me: My Journey to Web Development.",
-    bg: "#F1EBEC",
-    description:
-      "Hello, I'm Aya. Let me share with you my journey to becoming a passionate web developer. ",
-    link: "https://dev.to/aya02200220/about-me-mg8",
-  },
-];
+module.exports = {
+  ja: [
+    {
+      id: "5",
+      img: "/images/blog_images/5.jpg",
+      imgSmall: "/images/blog_images/small/5.jpg",
+      date: "Sep 16, 2023",
+      title:
+        "Overcoming Deployment Challenges with a MERN Stack application on Vercel",
+      bg: "#F1EBEC",
+      description:
+        "Deploying a MERN stack application, especially the server-side code, on platforms like Vercel can be a daunting task. Navigating through the myriad of configurations and dealing with unexpected issues can be overwhelming. In this article, I'll share the challenges I faced while deploying my MERN blog site's server-side code on Vercel and how I overcame them. ",
+      link: "https://dev.to/aya02200220/overcoming-deployment-challenges-with-a-mern-stack-blog-on-vercel-3p12",
+    },
+    {
+      id: "1",
+      img: "/images/blog_images/1.jpg",
+      imgSmall: "/images/blog_images/small/1.jpg",
+      date: "Aug 22, 2023",
+      category: "Inspiration",
+      title: "Why Isn't the Token Being Saved in the Cookie?",
+      bg: "#D5C5C8",
+      description:
+        "In web development, we occasionally come across issues that are puzzling and time-consuming to solve. One such issue that I recently faced involved JWT tokens not being stored in cookies for specific users. I invested several days trying to resolve this issue and eventually found a solution. In this blog post, I will share the details of the problem and the steps I took to resolve it.",
+      link: "https://dev.to/aya02200220/why-isnt-the-token-being-saved-in-the-cookie-1dmp",
+    },
+    {
+      id: "2",
+      img: "/images/blog_images/2.jpg",
+      imgSmall: "/images/blog_images/small/2.jpg",
+      date: "Jul 12, 2023",
+      title: "Quick Tips: Styling MUI Components with the sx Prop",
+      bg: "#F1EBEC",
+      description:
+        "Elevate Your MUI Components: Mastering the sx Prop for Styling Material-UI (now known as MUI) has always been a popular choice among React developers for its rich component library and flexibility. One of the game-changers introduced in MUI is the sx prop, which provides a powerful and concise way to style components directly. In today's quick tip, we'll explore how to use the sx prop to change button hover effects and to integrate images seamlessly into Box components.",
+      link: "https://dev.to/aya02200220/material-ui-1k03",
+    },
+    {
+      id: "3",
+      img: "/images/blog_images/3.jpg",
+      imgSmall: "/images/blog_images/small/3.jpg",
+      date: "Feb 10, 2023",
+      title: "Mastering the Basics: Manipulating the DOM with JavaScript",
+      bg: "#D5C5C8",
+      description:
+        "The Document Object Model (DOM) is a programming interface for HTML and XML documents that allows developers to manipulate web pages dynamically with JavaScript. By representing an HTML document as a tree structure, the DOM provides a convenient way to traverse, select, and modify elements on the page. In this article, we will cover the basics of how to manipulate the DOM with JavaScript. ",
+      link: "https://dev.to/aya02200220/we-have-to-kill-our-impostor-syndrome--40c3",
+    },
+    {
+      id: "4",
+      img: "/images/blog_images/4.jpg",
+      imgSmall: "/images/blog_images/small/4.jpg",
+      date: "Aug 31, 2022",
+      title: "About Me: My Journey to Web Development.",
+      bg: "#F1EBEC",
+      description:
+        "Hello, I'm Aya. Let me share with you my journey to becoming a passionate web developer. ",
+      link: "https://dev.to/aya02200220/about-me-mg8",
+    },
+  ],
+  en: [
+    {
+      id: "5",
+      img: "/images/blog_images/5.jpg",
+      imgSmall: "/images/blog_images/small/5.jpg",
+      date: "Sep 16, 2023",
+      title: "MERNスタックアプリケーションのVercelでのデプロイの課題を克服する",
+      bg: "#F1EBEC",
+      description:
+        "MERNスタックアプリケーション、特にサーバーサイドコードをVercelのようなプラットフォームにデプロイすることは大変な作業です。無数の設定をナビゲートし、予期しない問題に対処することは圧倒されることがあります。この記事では、私がMERNブログサイトのサーバーサイドコードをVercelにデプロイする際に直面した課題とその克服方法を共有します。",
+      link: "https://dev.to/aya02200220/overcoming-deployment-challenges-with-a-mern-stack-blog-on-vercel-3p12",
+    },
+    {
+      id: "1",
+      img: "/images/blog_images/1.jpg",
+      imgSmall: "/images/blog_images/small/1.jpg",
+      date: "Aug 22, 2023",
+      category: "Inspiration",
+      title: "なぜトークンがクッキーに保存されないのか？",
+      bg: "#D5C5C8",
+      description:
+        "ウェブ開発では、時々謎めいた時間のかかる問題に直面します。私が最近直面したそのような問題の一つは、特定のユーザーのためにJWTトークンがクッキーに保存されないことでした。この問題を解決するために数日間を費やし、最終的に解決策を見つけました。このブログ投稿では、その問題の詳細と解決に至るまでのステップを共有します。",
+      link: "https://dev.to/aya02200220/why-isnt-the-token-being-saved-in-the-cookie-1dmp",
+    },
+    {
+      id: "2",
+      img: "/images/blog_images/2.jpg",
+      imgSmall: "/images/blog_images/small/2.jpg",
+      date: "Jul 12, 2023",
+      title: "クイックヒント：sx Propを使ってMUIコンポーネントをスタイリング",
+      bg: "#F1EBEC",
+      description:
+        "MUIコンポーネントを高める：スタイリングのためのsx Propのマスターリング。Material-UI（現在はMUIとして知られています）は、リッチなコンポーネントライブラリと柔軟性でReact開発者の間で常に人気があります。MUIに導入されたゲームチェンジャーの一つは、コンポーネントを直接スタイリングするための強力で簡潔な方法を提供するsx propです。今日のクイックヒントでは、ボタンのホバーエフェクトを変更し、Boxコンポーネントに画像をシームレスに統合するためのsx propの使い方を探ります。",
+      link: "https://dev.to/aya02200220/material-ui-1k03",
+    },
+    {
+      id: "3",
+      img: "/images/blog_images/3.jpg",
+      imgSmall: "/images/blog_images/small/3.jpg",
+      date: "Feb 10, 2023",
+      title: "基礎をマスターする：JavaScriptを使ったDOMの操作",
+      bg: "#D5C5C8",
+      description:
+        "ドキュメントオブジェクトモデル（DOM）は、JavaScriptを使用してHTMLおよびXMLドキュメントを動的に操作するためのプログラミングインターフェースです。HTMLドキュメントをツリー構造として表現することにより、DOMはページ上の要素を移動、選択、および変更する便利な方法を提供します。この記事では、JavaScriptを使用してDOMを操作する方法の基本を紹介します。",
+      link: "https://dev.to/aya02200220/we-have-to-kill-our-impostor-syndrome--40c3",
+    },
+    {
+      id: "4",
+      img: "/images/blog_images/4.jpg",
+      imgSmall: "/images/blog_images/small/4.jpg",
+      date: "Aug 31, 2022",
+      title: "自己紹介：私のウェブ開発への旅",
+      bg: "#F1EBEC",
+      description:
+        "こんにちは、Ayaです。ウェブ開発に情熱を持つようになった私の旅を共有します。",
+      link: "https://dev.to/aya02200220/about-me-mg8",
+    },
+  ],
+};
